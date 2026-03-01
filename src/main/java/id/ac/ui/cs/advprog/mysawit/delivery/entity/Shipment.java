833583c7b0fc59b71fe78981b2f82778881edbd9
satriobismaa <bismazharfan@gmail.com>
2026-03-01 23:17:20 +0700

    feat: add shipment entity and adjust the dependency

---
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/delivery/entity/Shipment.java
@@ -0,0 +1,50 @@
+package id.ac.ui.cs.advprog.mysawit.delivery.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "shipments")
+public class Shipment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "plantation_id", nullable = false)
+    private UUID plantationId;
+
+    @Column(name = "mandor_id", nullable = false)
+    private UUID mandorId;
+
+    @Column(name = "driver_id", nullable = false)
+    private UUID driverId;
+
+    @Column(name = "total_weight_kg", nullable = false, precision = 10, scale = 2)
+    private BigDecimal totalWeightKg;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ShipmentStatus status = ShipmentStatus.MEMUAT;
+
+    @Column(name = "rejected_reason", columnDefinition = "TEXT")
+    private String rejectedReason;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
