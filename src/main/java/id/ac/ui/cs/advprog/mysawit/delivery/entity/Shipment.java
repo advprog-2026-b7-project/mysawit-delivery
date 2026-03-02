ad0872c8f91d01c86df4b4e9cfde4e97410517cc
satriobismaa <bismazharfan@gmail.com>
2026-03-02 23:38:15 +0700

    fear: make database for delivery work

--- src/main/java/id/ac/ui/cs/advprog/mysawit/delivery/entity/Shipment.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/delivery/entity/Shipment.java
@@ -7,7 +7,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
