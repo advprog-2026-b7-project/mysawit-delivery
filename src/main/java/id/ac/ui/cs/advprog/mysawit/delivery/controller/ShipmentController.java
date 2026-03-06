546319dcc0c445a6b172e092c3f7def793e8ff2d
satriobismaa <bismazharfan@gmail.com>
2026-03-06 19:34:34 +0700

    feat: add shipment controller

---
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/delivery/controller/ShipmentController.java
@@ -0,0 +1,39 @@
+package id.ac.ui.cs.advprog.mysawit.delivery.controller;
+
+import id.ac.ui.cs.advprog.mysawit.delivery.dto.CreateShipmentRequest;
+import id.ac.ui.cs.advprog.mysawit.delivery.dto.ShipmentResponse;
+import id.ac.ui.cs.advprog.mysawit.delivery.repository.ShipmentRepository;
+import id.ac.ui.cs.advprog.mysawit.delivery.service.ShipmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/deliveries")
+@RequiredArgsConstructor
+public class ShipmentController {
+
+    private final ShipmentService shipmentService;
+
+    @PostMapping
+    public ResponseEntity<ShipmentResponse> createShipment(@RequestBody CreateShipmentRequest request){
+        ShipmentResponse response = shipmentService.createShipment(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/{id}/assign-driver")
+    public ResponseEntity<ShipmentResponse> assignDriver(@PathVariable("id") UUID shipmentId,
+                                                         @RequestBody Map<String, UUID> requestBody){
+        UUID driverId = requestBody.get("driverId");
+        if (driverId == null) {
+            throw new IllegalArgumentException("Driver ID tidak boleh kosong!");
+        }
+
+        ShipmentResponse response = shipmentService.assignDriver(shipmentId, driverId);
+        return ResponseEntity.ok(response);
+    }
+}
