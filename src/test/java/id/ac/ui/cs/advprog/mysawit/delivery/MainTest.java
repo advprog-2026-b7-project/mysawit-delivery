20aa99f1e6fc9f4a9b546108b4e7094ebc417f77
satriobismaa <bismazharfan@gmail.com>
2026-05-17 11:11:54 +0700

    fix: main test not working

--- src/test/java/id/ac/ui/cs/advprog/mysawit/delivery/MainTest.java
+++ src/test/java/id/ac/ui/cs/advprog/mysawit/delivery/MainTest.java
@@ -11,6 +11,15 @@ class MainTest {
     @Test
     void mainRuns() {
         assertDoesNotThrow(() ->
-                MysawitDeliveryApplication.main(new String[]{}));
+                MysawitDeliveryApplication.main(new String[]{
+                        "--spring.flyway.enabled=false",
+                        "--spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+                        "--spring.datasource.driver-class-name=org.h2.Driver",
+                        "--spring.datasource.username=sa",
+                        "--spring.datasource.password=",
+                        "--spring.jpa.hibernate.ddl-auto=create-drop",
+                        "--spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+                        "--spring.profiles.active=test"
+                }));
     }
 }
