1f5de4f62a95dc4cd187de2f8436d40fa353b2e4
satriobismaa <bismazharfan@gmail.com>
2026-05-21 06:22:36 +0700

    chore: merge d03 into d05 and resolve conflicts correctly

--- src/test/java/id/ac/ui/cs/advprog/mysawit/delivery/controller/ShipmentControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/mysawit/delivery/controller/ShipmentControllerTest.java
@@ -31,12 +31,14 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @WebMvcTest(
         controllers = ShipmentController.class,
         excludeAutoConfiguration = {SecurityAutoConfiguration.class}
 )
+@AutoConfigureMockMvc(addFilters = false)
 @WithMockUser
 class ShipmentControllerTest {
 
